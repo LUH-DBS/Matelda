0218f4293068fe124c55afc785536dd5ab9ee0d4
Marc Speckmann <marc-speckmann@web.de>
2022-09-28 13:54:46 +0200

    Adding formating with black option to shell script

--- ED-SCALE.sh
+++ ED-SCALE.sh
@@ -1,1 +1,15 @@
-spark-submit --master local[*] end-to-end-eds.py
+case $1 in
+
+  'run')
+    spark-submit --master local[*] end-to-end-eds.py
+    ;;
+
+  'format')
+    black .
+    ;;
+
+  *)
+    echo "Enter valid command"
+    ;;
+esac
+    
