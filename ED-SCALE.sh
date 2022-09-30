bfd2c4478ffcecce3f3bad3924f8df09586a32e7
Marc Speckmann <marc-speckmann@web.de>
2022-09-30 12:23:48 +0200

    Adding options to the shell script

--- ED-SCALE.sh
+++ ED-SCALE.sh
@@ -1,11 +1,20 @@
 case $1 in
 
   'run')
+    conda activate Error-Detection-at-Scale
     spark-submit --master local[*] __main__.py
     ;;
 
   'format')
     black .
+    ;;
+
+  'install')
+    conda env create -f edatscale-env.yml
+    ;;
+
+  'activate')
+    conda activate Error-Detection-at-Scale
     ;;
 
   *)
