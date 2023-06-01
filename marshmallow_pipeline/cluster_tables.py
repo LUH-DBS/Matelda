662696a893949db70715e43438ca1f8012fcbe84
Marc Speckmann <marc-speckmann@web.de>
2023-06-01 15:39:02 +0200

    Uncommenting code in table clustering

--- marshmallow_pipeline/cluster_tables.py
+++ marshmallow_pipeline/cluster_tables.py
@@ -2,6 +2,7 @@
 import logging
 import os
 import pickle
+import subprocess
 
 import networkx.algorithms.community as nx_comm
 
@@ -84,27 +85,27 @@ def run_santos(
                     dirty_csv_path, os.path.join(santos_query_path, name + ".csv")
                 )
 
-    # logging.info("Santos run data_lake_processing_yago")
-    # # 1 == tus_benchmark
-    # marshmallow_pipeline.santos.codes.data_lake_processing_yago.main(1)
+    logging.info("Santos run data_lake_processing_yago")
+    # 1 == tus_benchmark
+    marshmallow_pipeline.santos.codes.data_lake_processing_yago.main(1)
 
-    # logging.info("Creating functinal dependencies/ground truth for santos")
-    # datalake_files = [os.path.join(santos_lake_path, file) for file in os.listdir(santos_lake_path) if file.endswith('.csv')]
-    # with open('marshmallow_pipeline/santos_fd/tus_datalake_files.txt', 'w+', encoding='utf-8') as file:
-    #     file.write('\n'.join(datalake_files))
+    logging.info("Creating functinal dependencies/ground truth for santos")
+    datalake_files = [os.path.join(santos_lake_path, file) for file in os.listdir(santos_lake_path) if file.endswith('.csv')]
+    with open('marshmallow_pipeline/santos_fd/tus_datalake_files.txt', 'w+', encoding='utf-8') as file:
+        file.write('\n'.join(datalake_files))
 
-    # process = subprocess.Popen(['bash', 'marshmallow_pipeline/santos_fd/runFiles.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # with process.stdout:
-    #     for line in iter(process.stdout.readline, b''): # b'\n'-separated lines
-    #         logging.info("Santos FD: %s", line.decode("utf-8").strip())
-    # process.wait()
+    process = subprocess.Popen(['bash', 'marshmallow_pipeline/santos_fd/runFiles.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    with process.stdout:
+        for line in iter(process.stdout.readline, b''): # b'\n'-separated lines
+            logging.info("Santos FD: %s", line.decode("utf-8").strip())
+    process.wait()
     
-    # logging.info("Santos run sortFDs_pickle_file_dict")
-    # marshmallow_pipeline.santos_fd.sortFDs_pickle_file_dict.main()
+    logging.info("Santos run sortFDs_pickle_file_dict")
+    marshmallow_pipeline.santos_fd.sortFDs_pickle_file_dict.main()
 
-    # logging.info("Santos run data_lake_processing_synthesized_kb")
-    # # 1 == tus_benchmark
-    # marshmallow_pipeline.santos.codes.data_lake_processing_synthesized_kb.main(1)
+    logging.info("Santos run data_lake_processing_synthesized_kb")
+    # 1 == tus_benchmark
+    marshmallow_pipeline.santos.codes.data_lake_processing_synthesized_kb.main(1)
 
     logging.info("Santos run query_santos")
     # 1 == tus_benchmark, 3 == full
